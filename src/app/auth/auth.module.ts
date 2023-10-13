076834d44a7e18f9c97157abf8ad3a0c877416f8
GeovannyTS <yowtecum@gmail.com>
2023-10-13 17:34:31 -0600

    Implements reactive Forms and styles login

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
 import { AuthLayoutComponent } from './layout/auth-layout/auth-layout.component';
+
 
 
 @NgModule({
@@ -15,7 +17,8 @@ import { AuthLayoutComponent } from './layout/auth-layout/auth-layout.component'
   ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    ReactiveFormsModule,
   ]
 })
 export class AuthModule { }
